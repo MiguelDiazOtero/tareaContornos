529bcdd525b71a5a24d8d671117e2744b79a92c6
Miguel Díaz <migueldiazotero@gmail.com>
2025-03-14 20:03:10 +0100

    Añadida la documentación Javadoc

--- src/cuenta/CCuenta.java
+++ src/cuenta/CCuenta.java
@@ -1,4 +1,8 @@
 package cuenta;
+/**
+ * La clase CCuenta permite realizar operaciones como
+ * ingresar y retirar dinero, así como consultar el saldo actual.
+ */
 public class CCuenta {
     private String nombre;
     private String cuenta;
@@ -15,19 +19,36 @@ public class CCuenta {
         setCuenta(cue);
         setSaldo(sal);
     }
-
+    /**
+     * Devuelve el saldo actual de la cuenta.
+     *
+     * @return El saldo actual de la cuenta.
+     */
     public double estado()
     {
         return getSaldo();
     }
-
+    /**
+     * Ingresa una cantidad de dinero en la cuenta. Si la cantidad es negativa,
+     * se lanzará una excepción.
+     *
+     * @param cantidad La cantidad de dinero a ingresar.
+     * @throws Exception Si la cantidad es negativa, se lanzará una excepción.
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    /**
+     * Retira una cantidad de dinero de la cuenta. Si la cantidad es negativa
+     * o si no hay suficiente saldo, se lanzará una excepción.
+     *
+     * @param cantidad La cantidad de dinero a retirar.
+     * @throws Exception Si la cantidad es negativa o si no hay suficiente saldo,
+     *                   se lanzará una excepción.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
